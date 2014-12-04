69efdf8bc472b19d4575b5a00ff04ad21300deba
Jacek Laskowski <jacek@japila.pl>
2014-12-05 00:04:40 +0100

    Basic Specs2 test (copied directly from specs2.org)

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,8 +1,16 @@
-import org.scalatest._
-import org.scalatest.matchers.ShouldMatchers
+import org.specs2._
 
-class HelloSpec extends FlatSpec with ShouldMatchers {
-  "Hello" should "have tests" in {
-    true should be === true
-  }
+class HelloWorldSpec extends Specification { def is = s2"""
+
+  This is a specification to check the 'Hello world' string
+
+  The 'Hello world' string should
+    contain 11 characters                             $e1
+    start with 'Hello'                                $e2
+    end with 'world'                                  $e3
+                                                      """
+
+  def e1 = "Hello world" must have size(11)
+  def e2 = "Hello world" must startWith("Hello")
+  def e3 = "Hello world" must endWith("world")
 }
