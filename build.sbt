af32a187730acfc16132bfdb59beea74622d6eb8
Jacek Laskowski <jacek@japila.pl>
2014-12-06 23:38:20 +0100

    Small test improvement + build change

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.4"
 
 libraryDependencies += "org.specs2" %% "specs2-core" % "2.4.14" % "test"
+
+scalacOptions in Test += "-Yrangepos"
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -6,11 +6,12 @@ class HelloWorldSpec extends Specification { def is = s2"""
 
   The 'Hello world' string should
     contain 11 characters                             $e1
+    ${ "Hello world" must have size 11 }
     start with 'Hello'                                $e2
     end with 'world'                                  $e3
                                                       """
 
-  def e1 = "Hello world" must have size(11)
+  def e1 = "Hello world" must have size 11
   def e2 = "Hello world" must startWith("Hello")
   def e3 = "Hello world" must endWith("world")
 }
