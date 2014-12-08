5f8796edfa8acd07b090d6834a78ec81279fe2d7
Tomasz Bartczak <kretes@gazeta.pl>
2014-12-08 23:02:35 +0100

    * moved loginPrefix and loadUser out of User class, as they do not belong to a specific User * kept only tests that do not introduce a conflicted universe * introudced and appending user module

--- src/main/scala/H2UserModule.scala
+++ src/main/scala/H2UserModule.scala
@@ -1,7 +1,11 @@
 trait H2UserModule extends UserModule {
+
   class User(val id: Long, val login: String) extends UserLike {
-    override def loginPrefix: String = "h2"
-    override def load(id: Long): User = new User(id, login)
   }
-  def User(id: Long, login: String): User = new User(id, login)
+
+  override def User(id: Long, login: String): User = new User(id, login)
+
+  override def loadUser(id: Long): User = User(id, loginPrefix)
+
+  override def loginPrefix: String = "h2"
 }
--- src/test/scala/CakePatternSpec.scala
+++ src/test/scala/CakePatternSpec.scala
@@ -8,10 +8,7 @@ class CakePatternSpec extends Specification { def is = s2"""
     return user with 'h2' login                   $e1
     return user with 'mysql' login                $eC
     return user with 'in-memory' login            $e2
-    return user with 'h2' login                   $e3
     return user with 'mysql' login                $eDHM
-    return user with 'h2' login                   $eDH
-    return user with 'h2' login...WHY not in-memory+doNothing?!  $eDHDN
     return user with 'in-memory+doNothing' login  $eDDN
     return user with 'in-memory+h2' login         $eCDMH
     ${ "Hello world" must have size 11 }
@@ -23,11 +20,8 @@ class CakePatternSpec extends Specification { def is = s2"""
 
   def e1 = universe.loadUser(1).login must_== "h2"
   def eC = universeC.loadUser(1).login must_== "mysql"
-  def e2 = (new H2UserModuleC with InMemoryUserModule).loadUser(1).login must_== "in-memory"
-  def e3 = (new H2UserModuleC with H2UserModule).loadUser(1).login must_== "h2"
-  def eDHM = (new DependentInMemoryUserModule with H2UserModule with MySQLUserModule {}).loadUser(1).login must_== "mysql"
-  def eDH = (new DependentInMemoryUserModule with H2UserModule {}).loadUser(1).login must_== "h2"
-  def eDHDN = (new DependentInMemoryUserModule with H2UserModule with DoNothingUserModule {}).loadUser(1).login must_== "h2"
-  def eDDN = (new DependentInMemoryUserModule with DoNothingUserModule {}).loadUser(1).login must_== "in-memory+doNothing"
-  def eCDMH = (new DependentInMemoryUserModuleC with DependentInMemoryUserModule).loadUser(1).login must_== "in-memory+doNothing"
+  def e2 = new InMemoryUserModule {}.loadUser(1).login must_== "in-memory"
+  def eDHM = (new H2UserModule with AppendingUserModule).loadUser(1).login must_== "h2+appending"
+  def eDDN = (new DoNothingUserModule with AppendingUserModule).loadUser(1).login must_== "doNothing+appending"
+  def eCDMH = (new DependentInMemoryUserModule with AppendingUserModule ).loadUser(1).login must_== "doNothing+appending"
 }
